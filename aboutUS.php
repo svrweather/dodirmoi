b6084ae6a00282afd9a23ef9e6e8a68c63dcb25e
unknown <damir-developer@yandex.ru>
2023-01-20 19:30:20 +0300

    fixx

--- aboutUS.php
+++ aboutUS.php
@@ -8,14 +8,15 @@
  <html lang="en">
  <head>
  	<meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1">
    <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet" href="css/media1.css">
+ 	<link rel="stylesheet" href="css/style.css">
+     <link rel="stylesheet" href="css/diagrams.css">
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!--     <link rel="stylesheet" href="css/jquery.circliful.css">-->
+     <link href="css/main.css" rel="stylesheet" type="text/css" />
 
   <link 
   rel="stylesheet"
@@ -30,8 +31,7 @@
  </head>
  <body>
   <div class="container">
-    <div class="headerMedia">
-      <header>
+    <header>
       <div>
         <h1 class="content-header_title">мойдодыр</h1>
       </div>
@@ -39,42 +39,66 @@
         <img src="css/logo.png" alt="(((">
       </div>
       <div class="userName">
-        Дамир Рахматуллин
       </div>
       <div class="profile_pic">
-        <img src="css/images.jpeg" alt=")))">
+        <img src="css/user.jpg" alt=")))">
       </div>
     </header>
-    </div>
-    
-    <div class="mainPartContainer">
-      <div class="navigationMedia">
-          <div class="navigation">
-            <ul class="nav">
-                <li><a href="/personalAccount.php">Мой профиль</a></li>
-                <li><a href="/howItWorks.php">Как это работает?</a></li>
-                <li class="bold"><a href="/aboutUs.php">О нас</a></li>
-                <li><a href="#">Статистика</a></li>
-                <li><a href="/news.php">Новости</a></li>
-            </ul>
+      <div class="mainPartContainer">
+        <div class="navigation">
+          <ul class="nav">
+              <li><a href="/authorized.php">Мой профиль</a></li>
+              <li><a href="/howItWorks.php">Как это работает?</a></li>
+              <li class="bold"><a href="/aboutUS.php">О нас</a></li>
+              <li><a href="/myStatistic.php">Статистика</a></li>
+              <li><a href="/news.php">Новости</a></li>
+              <li><a href="/admin/admin.php">Админ-панель</a></li>
+          </ul>
         </div>
-      </div>
-      <div class="sideContentMedia">
-        <div class="sideContent">
-            <p class="contentArticle">
-            Имеется множество объектов (ситуаций) и множество возможных ответов (откликов, реакций). Существует некоторая зависимость между ответами и объектами, но она неизвестна. Известна только конечная совокупность прецедентов — пар «объект, ответ», называемая обучающей выборкой. На основе этих данных требуется восстановить неявную зависимость, то есть построить алгоритм, способный для любого возможного входного объекта выдать достаточно точный классифицирующий ответ. Эта зависимость не обязательно выражается аналитически, и здесь нейросети реализуют принцип эмпирически формируемого решения.
-            </p>
-            
-        </div>
-      </div>
-        
+        <div class="sideContent-about">
+        	<div class="top-content">
+        		<h1>Наша команда</h1>
+        		<p align="center">Мы - команда школьников и студентов,
+				каждый из нас имеет достижения в своей области.
+				Мы готовы бороться с неправильным мытьём рук !</p>
+        	</div>
+        	
+	         <div class="peopleGrid">
+	            <article><div class="artContainer">
+	              <img src="css/user.jpg" alt="" style="width: 220px; height: 220px;border-radius: 30px; align-content: center; margin-right: auto; margin-left: auto; display: block;">
+	              <h1>Никита Сентяков</h1>
+	              <p>Генеральный директор, Team Leader</p>
+	            </div></article>
+	          <article><div class="artContainer">
+	              <img src="css/anna.jpg" alt="" style="width: 220px; height: 220px;border-radius: 30px; align-content: center; margin-right: auto; margin-left: auto; display: block;">
+	              <h1>Анна Рябчикова</h1>
+	              <p>Финансовый директор, директор по маркетингу</p>
+	            </div></article>
+	            <article><div class="artContainer">
+	              <img src="css/damir.jpg" alt="" style="width: 220px; height: 220px;border-radius: 30px; align-content: center; margin-right: auto; margin-left: auto; display: block;">
+	              <h1>Дамир Рахматуллин</h1>
+	              <p>Web-Разработчик</p>
+	            </div></article>
+	            <article><div class="artContainer">
+	              <img src="css/bogdan.jpg" alt="" style="width: 220px; height: 220px;border-radius: 30px; align-content: center; margin-right: auto; margin-left: auto; display: block;">
+	              <h1>Богдан Урсегов</h1>
+	              <p>Backend-разработчик</p>
+	            </div></article>
+	            <article><div class="artContainer">
+	              <img src="css/roma.jpg" alt="" style="width: 220px; height: 220px;border-radius: 30px; align-content: center; margin-right: auto; margin-left: auto; display: block;">
+	              <h1>Роман Лашкарев</h1>
+	              <p>Web-Разработчик</p>
+	            </div></article>
+	            <article><div class="artContainer">
+	              <img src="css/andrei.jpg" alt="" style="width: 220px; height: 220px;border-radius: 30px; align-content: center; margin-right: auto; margin-left: auto; display: block;">
+	              <h1>Андрей Крюков</h1>
+	              <p>Data Scientis</p>
+	            </div></article>
+	        </div>
+	    </div>
     </div>
-    <script>
-  function onClickMenu(a) {
-    document.getElementById(a).classList.toggle("change");
-
-  }
-  src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"
- </script>
-
-<!-- onclick="onClickMenu()" -->
+ </body>
+ <footer>
+   
+ </footer>
+ <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
