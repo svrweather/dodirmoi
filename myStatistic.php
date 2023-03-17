deb04a6cbc977982f8e665a0138fe6bbda026d6e
unknown <damir-developer@yandex.ru>
2023-03-18 00:09:38 +0300

    message

--- myStatistic.php
+++ myStatistic.php
@@ -8,6 +8,7 @@
  <html lang="en">
  <head>
  	<meta charset="UTF-8">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1">
    <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
@@ -29,9 +30,21 @@
   referrerpolicy="no-referrer"
   />  
   <script src="//cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js"></script>
- 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+ 	
  	<title>Document</title>
  </head>
+<!--  <style>
+ 	@media only screen and (max-width: 650px) {
+	    .menu {
+	        opacity: 1;
+	    }
+	}
+	@media only screen and (min-width: 700px){
+	    .menu {
+	        opacity: 0;
+	    }
+	}
+ </style> -->
  <body>
   <div class="container">
     <header>
