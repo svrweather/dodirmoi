8015b89d3e1c06ee927ca012361bb79f89e0cf2e
unknown <damir-developer@yandex.ru>
2023-03-18 00:36:29 +0300

    new updates

--- functions/connect.php
+++ functions/connect.php
@@ -1,5 +1,5 @@
 <?php 
-	$connect = mysqli_connect('localhost', 'root', '', 'project');
+	$connect = mysqli_connect('u207573.mysql.masterhost.ru', 'u207573', 'tIcOtO.IciaT2l', 'u207573_hccs');
 
 	if (!$connect){
 		die('Error connect to database');
